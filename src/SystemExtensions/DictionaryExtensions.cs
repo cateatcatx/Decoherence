ab3a24d7a60af7a77d0525b9ff557754e0badd62
harveyjin <harveyjin@tencent.com>
2021-12-31 22:40:44 +0800

    c

---
+++ src/SystemExtensions/DictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Decoherence.SystemExtensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue AddOrCreateValue<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, Func<TValue> createFunc)
+        {
+            if (!self.TryGetValue(key, out var value))
+            {
+                value = createFunc();
+                self.Add(key, value);
+            }
+
+            return value;
+        }
+        
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue value)
+        {
+            if (!self.ContainsKey(key))
+            {
+                self.Add(key, value);
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
